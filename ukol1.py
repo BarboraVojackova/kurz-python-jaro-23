8a7cea25850380fce6ebbc270e742ce11d1b3d78
Barbora Vojackova <barbaravojackova@gmail.com>
2023-04-03 22:04:46 +0200

    úkol 1

--- ukol1.py
+++ ukol1.py
@@ -1,16 +1,17 @@
 # úkol:
 jmeno="Barbora"
-email = jmeno + "@czechitas.cz"
+email=jmeno+"@czechitas.cz"
 print(email)
 
 # nepovinný bonus:
 jmeno_a_prijmeni=input("Zadejte své jméno a příjmení:")
-jmeno, prijmeni = jmeno_a_prijmeni.split()
 print(jmeno_a_prijmeni.upper())
 print(jmeno_a_prijmeni.lower())
-print(jmeno.capitalize(), prijmeni.capitalize())
-print(jmeno[0].upper() + ".", prijmeni[0].upper() + ".")
-if len(jmeno) > 5:
-    print(jmeno[0].upper() + ".", prijmeni.capitalize())
+# rozdělím jméno a příjmení
+jmeno, prijmeni=jmeno_a_prijmeni.split()
+print(jmeno.capitalize(),prijmeni.capitalize())
+print(jmeno[0].upper()+".",prijmeni[0].upper()+".")
+if len(jmeno)>5:
+    print(jmeno[0].upper()+".",prijmeni.capitalize())
 else:
-    print(jmeno.capitalize(), prijmeni.capitalize())
+    print(jmeno.capitalize(),prijmeni.capitalize())
