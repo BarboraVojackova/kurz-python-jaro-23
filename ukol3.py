6fb3aecebd8e7dd979a980ab2a31cd0d3a3070b4
Barbora Vojackova <barbaravojackova@gmail.com>
2023-04-25 00:02:45 +0200

    úkol č. 3

---
+++ ukol3.py
@@ -0,0 +1,21 @@
+# 1. Import a načtení
+import json
+with open('body.json', encoding='utf-8') as file:
+    points = json.load(file)
+
+prospech = {}
+
+# Stanovené podmínky 60 bodů
+for jmeno, body in points.items():
+    if body >= 60:
+        prospech[jmeno] = 'Pass'
+    else:
+        prospech[jmeno] = 'Fail'
+# print(prospech)
+
+# Vyvoření nového JSON souboru
+import json
+prospech = {}
+
+with open ("prospech.json", mode="w", encoding="utf-8") as soubor:
+    json.dump(prospech, soubor, ensure_ascii=False)
